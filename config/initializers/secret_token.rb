d995d1e2a2c21dbb58a80425cc96ff9026a6555d
jeremy <jdicasolo@gmail.com>
2010-11-22 16:07:24 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DailyHarbinger::Application.config.secret_token = '472151513168e8738d9e4f6bad69af60bc336c50a9653cb26e490bc2d6917aa6302e91999c486f5323bbdef03efcbaba26ef2c94389ef8db8d5e90a6db69671e'
---
+++ features/support/env.rb
@@ -0,0 +1,3 @@
+#placeholder to tell cucumber we're using ruby
+
+$LOAD_PATH << File.expand_path('../../../app', __FILE__)
