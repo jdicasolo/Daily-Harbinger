82df38930eacd701d19008f5f82e166126cafb55
jeremy <jdicasolo@gmail.com>
2010-11-29 09:46:42 -0500

    removed default rails page,  changed route mappings to use helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def title
+    base_title = "Daily Harbinger"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+
+
 end
--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,2 +1,9 @@
 Autotest.add_discovery { "rails" }
 Autotest.add_discovery { "rspec2" }
+
+#Autotest.add_hook :initialize do |autotest|
+#  autotest.add_mapping ('/^spec\/requests\/.*_spec\.rb$') do
+#    autotest.files_matching ('/^spec\/requests\/.*spec\.rb$')
+#  end
+#end
+
