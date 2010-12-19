a01e399904ae29802006a686cbf6de1dd700691b
jeremy <jdicasolo@gmail.com>
2010-12-19 14:42:37 -0500

    user signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,25 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
     @title = "Sign Up"
   end
 
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      flash[:success] = "Welcome"
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,68 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  describe "POST 'create'" do
+
+    describe "success" do
+
+      before(:each) do
+        @attr = { :name => "New User", 
+                  :email => "user@email.com", 
+                  :password => "foobar", 
+                  :password_confirmation => "foobar" }
+      end
+
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome/i
+      end
+
+
+    end
+
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = { :name => "", 
+                  :email => "", 
+                  :password => "", 
+                  :password_confirmation => "" }
+      end
+
+
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign Up")
+      end
+
+      it "should render the 'new' page" do 
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+
+    end
+
+  end
+
+
   describe "GET 'new'" do
 
     it "should be successful" do
